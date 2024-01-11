a7723c3b11e1fdefa4e9eb1dc8c5b9f248a48af4
EricaKl <eriKrishna21@gmail.com>
2024-01-11 18:34:07 +0530

    Add project files.

---
+++ NumericFunctions.cs
@@ -0,0 +1,190 @@
+﻿namespace Assignment5
+{
+    public class NumericFunctions
+    {
+        public NumericFunctions()
+        {
+            Console.WriteLine("WELCOME");
+        }
+        public int Add(params int[] numbers)
+        {
+            int sum = 0;
+            try
+            {
+                foreach (int i in numbers)
+                {
+                    sum += i;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return sum;
+        }
+        public int Sub(params int[] numbers)
+        {
+            int sub = 0;
+            int result;
+            try
+            {
+                foreach (int i in numbers)
+                {
+                    sub = i - sub;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return sub;
+        }
+        public int Mul(params int[] numbers)
+        {
+            int mul = 1;
+            try
+            {
+                foreach (int i in numbers)
+                {
+                    mul *= i;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return mul;
+        }
+
+        public float Div(float n1, float n2)
+        {
+            float div = 10f;
+
+            try
+            {
+                div = n1 / n2;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            return div;
+        }
+
+        public double Max(params double[] numbers)
+        {
+            double max = 0;
+            try
+            {
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    max = numbers[0];
+                    if (max < numbers[i])
+                    {
+                        max = numbers[i];
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        
+            return max;
+
+        }
+
+        public double Min(params double[]numbers )
+        {
+            double min = 0;
+            try
+            {
+                min = numbers[0];
+                for(int i=1;i<numbers.Length; i++)
+                {
+                    if(min > numbers[i])
+                    {
+                        min = numbers[i];
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return min;
+        }
+
+        public bool IsEven(int n)
+        {
+            bool isEven = false;
+            try
+            {
+                if(n%2 == 0)
+                {
+                    isEven = true;
+                    //return true;
+                }
+                else
+                {
+                    isEven = false;
+                    //return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isEven;
+        }
+
+        public bool IsOdd(int n)
+        {
+            bool isOdd = false;
+            try
+            {
+                if (n % 2 != 0)
+                {
+                    isOdd = true;
+                    //return true;
+                }
+                else
+                {
+                    isOdd = false;
+                    //return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isOdd;
+        }
+        public bool IsPrime(int number)
+        { 
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
+
---
+++ StringFunctions.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment5
+{
+    public  class StringFunctions
+    {
+        public int NoOfCharInSentence(string sentence)
+        {
+           
+            return sentence.Length;
+            
+        }
+
+        public bool CheckForPallindrome(string sentence)
+        {
+            //
+            //string result= (string)sentence.Reverse();
+            string result ="";
+            bool flag = false;
+            try
+            {
+                
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    result += sentence[i];
+                }
+
+                if (sentence.ToUpper() == result.ToUpper())
+                {
+                    flag = true;
+                   // return true;
+                }
+                else
+                {
+                    flag = false;
+                    //return false;
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return flag;
+        }
+        public string ReverseSentence(string sentence)
+        {
+            string result = "";
+            try
+            {
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    result += sentence[i];
+                }
+            }
+            catch( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
+           
+            return result;
+
+        }
+
+        public void FindAllTypesChar(string sentence/*out int vowels, out int consonants , out int integers, out int special*/)
+        {
+           
+            //char[] arr = sentence.ToCharArray();
+            int consonants = 0;
+            int vowels = 0;
+            int integers = 0;
+            int special = 0;
+            try
+            {
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] >= 'A' && sentence[i] <= 'Z' || sentence[i] >= 'a' && sentence[i] <= 'z')
+                    {
+                        if (sentence[i] == 'A' || sentence[i] == 'a' || sentence[i] == 'E' || sentence[i] == 'e'
+                            || sentence[i] == 'I' || sentence[i] == 'i' || sentence[i] == 'O' || sentence[i] == 'o' ||
+                            sentence[i] == 'U' || sentence[i] == 'u')
+                        {
+                            vowels++;
+                        }
+                        else
+                        {
+                            consonants++;
+                        }
+
+                    }
+                    if (sentence[i] >= '1' && sentence[i] <= '9')
+                    {
+                        integers++;
+                    }
+                    if (sentence[i] == '@' || sentence[i] == ' ' || sentence[i] == '#' || sentence[i] == '$')
+                    {
+                        special++;
+                    }
+
+                }
+                Console.WriteLine("No of integers : " + integers);
+                Console.WriteLine("No of Vowels : " + vowels);
+                Console.WriteLine("No of Consonants : " + consonants);
+                Console.WriteLine("No of Special Characters : " + special);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+          
+        }
+
+        public string StringToUpper(string sentence)
+        {
+            return sentence.ToUpper();
+
+        }
+        public string StringToProper(string sentence)
+        {
+            var textinfo = CultureInfo.CurrentCulture.TextInfo;
+            return textinfo.ToTitleCase(sentence);
+
+        }
+
+        public string CombineTwoSentences(string sentence1 , string sentence2)
+        {
+           
+
+            return sentence1 = sentence1 + " " +sentence2;
+
+        }
+
+        public string RemoveExtraSpaces(string sentence)
+        {
+            return Regex.Replace(sentence, @"\s+", " ").Trim();
+        }
+
+        public int CountNoOfWords(string sentence)
+        {
+           // char[] arr = sentence.ToCharArray();
+            int count = 0;
+            try
+            {
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i].Equals(' '))
+                    {
+                        count++;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return count;
+        }
+
+        public bool FindSubstring(string sentence, string key)
+        {
+            bool flag = false;
+            try
+            {
+                if (sentence.Contains(key))
+                {
+                    flag = true;
+                    //return true;
+                }
+                else
+                {
+                    flag = false;
+                    //return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return flag;
+        }
+
+        public int OccurrenceSubString(string sentence, string Key)
+        {
+            int count = 0;
+            try
+            {
+                for (int i = 0; i < sentence.Length - Key.Length + 1; i++)
+                {
+                    if (sentence.Substring(i, Key.Length) == Key)
+                    {
+                        count++;
+                    }
+                }
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+           
+            return count;
+        }
+
+
+
+    }
+}
